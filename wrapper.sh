922c63aa911a26b5602855a1b47b29d52f34302f
Maya Rashish <maya@NetBSD.org>
2021-02-08 22:11:31 +0200

    Match autobuild: no T, no seconds

--- wrapper.sh
+++ wrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-date=$(TZ=UTC date -r $(cd ../src; git show -s --format=%ct) +%Y%m%dT%H%M%SZ)
+date=$(TZ=UTC date -r $(cd ../src; git show -s --format=%ct) +%Y%m%d%H%MZ)
 ../src/build.sh -j$(sysctl -n hw.ncpuonline) -B $date $@
