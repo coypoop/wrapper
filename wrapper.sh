2d8a5e3deec1c23fefa4d9bfcf9b46d5479b3913
Maya Rashish <maya@NetBSD.org>
2021-02-08 23:09:09 +0200

    More hacks, unclear why a simple "-M obj" behaves weird.

--- wrapper.sh
+++ wrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 date=$(TZ=UTC date -r $(cd ../src; git show -s --format=%ct) +%Y%m%d%H%MZ)
-../src/build.sh -j$(sysctl -n hw.ncpuonline) -B $date $@
+../src/build.sh -j$(sysctl -n hw.ncpuonline) -B $date -M $PWD/obj/ $@
